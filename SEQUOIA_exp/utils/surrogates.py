787179dd25e6b43dacc5742d32e3fc0c99fdc18f
Octavio M. <octavioocta234@gmail.com>
2026-08-31 23:26:37 +0200

    Delete SEQUOIA_exp directory

--- SEQUOIA_exp/utils/surrogates.py
+++
@@ -1,7 +0,0 @@
-#Dictionary with surrogates/approximants supported by SEQUOIA
-
-surrogates = {'non-precessing': ['DANSur', 'IMRPhenomXHM'] , 
-              
-              'precessing' : ['IMRPhenomXO4a']
-              
-              }
